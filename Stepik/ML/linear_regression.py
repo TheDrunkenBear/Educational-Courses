c1f36f47c02d8001601185b84bd3cdd6961eb7cc
Aleksandr Shilov <tabbols95@gmail.com>
2025-09-20 22:22:04 +0300

    feat: add method __str__

--- Stepik/ML/linear_regression.py
+++ Stepik/ML/linear_regression.py
@@ -11,3 +11,6 @@ class MyLineReg:
     def __init__(self, **kwargs) -> None:
         self.n_iter = kwargs.get("n_iter", 100)
         self.learning_rate = kwargs.get("learning_rate", 0.1)
+
+    def __str__(self):
+        return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
