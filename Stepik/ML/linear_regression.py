f5d89e6f11ab2d4233013c17232810499e0ec3ee
Aleksandr Shilov <tabbols95@gmail.com>
2025-09-20 22:18:40 +0300

    Init class MyLineReg

---
+++ Stepik/ML/linear_regression.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+
+class MyLineReg:
+    n_iter: Optional[int] = None
+    """Количество итераций градиентного спуска"""
+
+    learning_rate: Optional[float] = None
+    """Шаг градиентного спуска"""
+
+    def __init__(self, **kwargs) -> None:
+        self.n_iter = kwargs.get("n_iter", 100)
+        self.learning_rate = kwargs.get("learning_rate", 0.1)
